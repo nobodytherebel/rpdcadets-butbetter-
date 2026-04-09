4aaf3b5745694a2335cb98305654132a07d4daae
nobodytherebel <64049816+nobodytherebel@users.noreply.github.com>
2026-04-08 20:44:02 -0500

    Update shared.js

--- shared.js
+++ shared.js
@@ -129,3 +129,27 @@ document.addEventListener('DOMContentLoaded', () => {
   buildNav();
   buildFooter();
 });
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDm3oPI6qBNJWQErlTPD6RY6tnrXrJGby4",
+    authDomain: "test-e0036.firebaseapp.com",
+    databaseURL: "https://test-e0036-default-rtdb.firebaseio.com",
+    projectId: "test-e0036",
+    storageBucket: "test-e0036.firebasestorage.app",
+    messagingSenderId: "1068764877683",
+    appId: "1:1068764877683:web:174fd0dd6dc87fd960ab06",
+    measurementId: "G-W4WFVBFFG7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
